1c081002dbd003b297581f3cc2e8be25b46c8995
Spencer Kee <spencerkee7@gmail.com>
2016-04-29 02:01:05 -0400

    Added approximate fitness function and support for infinte generations

--- appliedEP.py
+++ appliedEP.py
@@ -149,13 +149,17 @@ def simpleGeneticAlgorithm(keyboard_list, fitness_list, elite_num, death_percent
 			parent1 = keyboard_list[fitness_list.index(simpleRouletteSelection(fitness_list))]
 			parent2 = keyboard_list[fitness_list.index(simpleRouletteSelection(fitness_list))]
 			child = singlePointCrossover(parent1,parent2)
-			if stringFitnesses(theInput, [child])[0] < sum(stringFitnesses(theInput, [parent1,parent2]))/2:
-				returnList.append(child)
-				break
-			else:
-				if random.uniform(0,100) >= death_percent:
-					returnList.append(child)
-					break
+
+			returnList.append(child)
+			break
+
+			# if stringFitnesses(theInput, [child])[0] < sum(stringFitnesses(theInput, [parent1,parent2]))/2:
+			# 	returnList.append(child)
+			# 	break
+			# else:
+			# 	if random.uniform(0,100) >= death_percent:
+			# 		returnList.append(child)
+			# 		break
 
 	# returnList = mutateKeyboards(returnList, 40, 3)
 	returnList = mutateKeyboards(returnList, 2, 1)
@@ -181,7 +185,8 @@ def gradientDescent():#should add a method to input something and see if any swi
 	possSwaps = list(itertools.combinations(range(len(letterString)), 2))
 	random.shuffle(possSwaps)
 	keyboard = createNKeyboards(1)[0]
-	previousBest = stringFitnesses(theInput, [keyboard])[0]
+	# previousBest = stringFitnesses(theInput, [keyboard])[0]
+	previousBest = frequencyFitness(keyboard)
 	i = 0
 	while i < len(letterString):
 		lst = list(keyboard)
@@ -190,8 +195,10 @@ def gradientDescent():#should add a method to input something and see if any swi
 		k = indices[1]
 		lst[j], lst[k] = lst[k], lst[j]
 		lst = (''.join(lst))
-		if stringFitnesses(theInput, [lst])[0] < previousBest:
-			previousBest = stringFitnesses(theInput, [lst])[0]
+		# if stringFitnesses(theInput, [lst])[0] < previousBest:
+		# 	previousBest = stringFitnesses(theInput, [lst])[0]
+		if frequencyFitness(lst) < previousBest:
+			previousBest = frequencyFitness(lst)
 			keyboard = lst
 			i = 0
 			random.shuffle(possSwaps)
@@ -206,10 +213,13 @@ def main():
 	if len(sys.argv) != 3:
 		sys.exit('Usage: ' + sys.argv[0] + ' [numKeyboards] [numGenerations]')
 	numKeyboards = int(sys.argv[1])
-	numGenerations = int(sys.argv[2])
+	if sys.argv[2] == 'inf':
+		numGenerations = float('inf')
+	else:
+		numGenerations = int(sys.argv[2])
 
-	# newPopulation= createNKeyboards(numKeyboards)
-	newPopulation = [gradientDescent() for i in range(50)]
+	newPopulation= createNKeyboards(numKeyboards)
+	# newPopulation = [gradientDescent() for i in range(50)]
 	i = 0
 	bestScore = float('inf')
 	bestKeyboard = ''
@@ -222,7 +232,8 @@ def main():
 		if i == numGenerations:
 			print("--- %s seconds ---" % (time.time() - start_time))
 			break
-		fitnesses = stringFitnesses(theInput, newPopulation)
+		# fitnesses = stringFitnesses(theInput, newPopulation)
+		fitnesses = [frequencyFitness(keyboard) for keyboard in newPopulation]
 		avg = sum(fitnesses)/len(fitnesses)
 		minIndex = fitnesses.index(min(fitnesses))
 
@@ -230,7 +241,8 @@ def main():
 			bestScore = min(fitnesses)
 			bestKeyboard = str(newPopulation[minIndex])
 		count = Counter(newPopulation)
-		print rowFormat.format(i, avg, min(fitnesses), newPopulation[minIndex], bestScore, bestKeyboard, count.most_common()[0]), len(count.most_common())
+		if i % 100 == 0:
+			print rowFormat.format(i, avg, min(fitnesses), newPopulation[minIndex], bestScore, bestKeyboard, count.most_common()[0]), len(count.most_common())
 
 		newPopulation = simpleGeneticAlgorithm(newPopulation, fitnesses, 1, death_percent=80)
 		newPopulation = diversify(4, newPopulation)
@@ -238,11 +250,138 @@ def main():
 		i += 1
 	print newPopulation[0]
 
+def frequencyFitness(keyboard):
+	fitness_score = 0
+	# a	b	c	d	e	f	g	h	i	j	k	l	m
+	# 8.2	1.5	2.8	4.3	12.7	2.2	2.0	6.1	7.0	0.2	0.8	4.0	2.4
+	# n	o	p	q	r	s	t	u	v	w	x	y	z
+	# 6.7	7.5	1.9	0.1	6.0	6.3	9.1	2.8	1.0	2.4	0.2	2.0	0.1
+	coordinates = [[0,3],[1,3],[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],[8,3],[9,3],
+	[0.5,2],[1.5,2],[2.5,2],[3.5,2],[4.5,2],[5.5,2],[6.5,2],[7.5,2],[8.5,2],
+	[0,0],[1.5,1],[2.5,1],[3.5,1],[4.5,1],[5.5,1],[6.5,1],[7.5,1],
+	[5.5,0]]
+	# freq_dict = {'a':8.2,'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7,'f':2.2,'g':2.0,'h':6.1,'i':7.0,'j':0.2,'k':0.8,'l':4.0,'m':2.4,
+	# 'n':6.7,'o':7.5,'p':1.9,'q':0.1,'r':6.0,'s':6.3,'t':9.1,'u':2.8,'v':1.0,'w':2.4,'x':0.2,'y':2.0,'z':0.1, '^':2.8,' ':13.6}
+	
+	freq_dict = {'a':8.50357834949,'b':1.58827987693,'c':3.35831867318,'d':3.8332505962,'e':12.0854920097,'f':2.1435268052,'g':1.99366445597,
+'h':4.72281799311,'i':7.28576061687,'j':0.221705546931,'k':0.778118038872,'l':4.07968142936,'m':2.64836003734,'n':7.27089968004,
+'o':7.41507990637,'p':2.14681185439,'q':0.101035966795,'r':6.57070724088,'s':6.88783871261,'t':8.94593736877,'u':2.56241658647,
+'v':1.04965603373,'w':1.72204517838,'x':0.201144083561,'y':1.77340052726,'z':0.110472431636, ' ':22.2596873, '^':5.41006961}
+
+
+	for i in keyboard:
+		left_distance = distance(coordinates[12],coordinates[keyboard.index(i)])
+		right_distance = distance(coordinates[16],coordinates[keyboard.index(i)])
+		fitness_score += min(left_distance,right_distance)*freq_dict[i]
+
+	# bigrams = {'th': 1.52,'en': 0.55,'ng': 0.18,'he':1.28,'ed':0.53,'of':0.16,'in':0.94,'to':0.52,'al':0.09,'er':0.94,'it': 0.50,'de':0.09,
+	#  'an':0.82,'ou':0.50,'se': 0.08,'re': 0.68,'ea': 0.47,'le': 0.08,'nd': 0.63,'hi': 0.46,'sa': 0.06,'at': 0.59,'is': 0.46,'si': 0.05,
+	#  'on': 0.57,'or': 0.43,'ar': 0.04,'nt': 0.56,'ti': 0.34,'ve': 0.04,'ha': 0.56,'as': 0.33,'ra': 0.04,'es': 0.56,'te': 0.27,'ld': 0.02,'st': 0.55,'et': 0.19,'ur': 0.02}
+
+	return fitness_score
+
 if __name__ == '__main__':
 	theInput = processText(theInput)
-	# print theInput
-	# main()
-	# while True:
+
+
+	# min2 = 'qforbvlwkx^teuya hjzingpdscm'
+	# print frequencyFitness(min2)
+	# print frequencyFitness('zpl^wyshgqvo rmieakxctdbfnuj')
+	# print frequencyFitness('qwertyuiopasdfghjkl^zxcvbnm ')
+
+	# print ''
+	# print stringFitnesses(theInput, ['qysrufhdwjvnaebo igxmtpzc^lk'])[0]
+	# print stringFitnesses(theInput, ['zpl^wyshgqvo rmieakxctdbfnuj'])[0]
+	# print stringFitnesses(theInput, ['qwertyuiopasdfghjkl^zxcvbnm '])[0]
+
+
+	# random_keyboards = createNKeyboards(50)
+	# print random_keyboards
+	# x = [frequencyFitness(keyboard) for keyboard in random_keyboards]
+	# print x
+	# print ''
+	# seq = sorted(x)
+	# index = [seq.index(v) for v in x]
+	# print index
+
+	# x = [stringFitnesses(theInput, [keyboard])[0] for keyboard in random_keyboards]
+	# print x
+	# print ''
+	# seq = sorted(x)
+	# index2 = [seq.index(v) for v in x]
+	# print index2
+
+	# total_difference = 0
+	# for i in range(50):
+	# 	total_difference += abs(index[i]-index2[i])
+	# print total_difference/50
+
+
+
+
+
+
+	# letters = collections.Counter(theInput)
+	# for i in letters:
+	# 	letters[i] = letters[i]/32.59
+	# print letters
+	# print len(theInput)
+
+
+	main()
+# 
+
+	# for i in range(10):
 	# 	x = gradientDescent()
-	# 	if stringFitnesses(theInput, [x])[0] < 1.25:
-	# 		print x
+	# 	print x, frequencyFitness(x)
+
+	
+	# print stringFitnesses(theInput, [min2])[0]
+
+
+
+
+
+# x = [
+# 'A', 280937 ,5263779, 
+# 'B', 169474 ,866156, 
+# 'C', 229363 ,1960412, 
+# 'D', 129632 ,2369820, 
+# 'E', 138443 ,7741842, 
+# 'F', 100751, 1296925, 
+# 'G', 93212 ,1206747,
+# 'H', 123632 ,2955858 ,
+# 'I', 223312 ,4527332,
+# 'J', 78706 ,65856 ,
+# 'K', 46580 ,460788 ,
+# 'L', 106984 ,2553152,
+# 'M', 259474 ,1467376,
+# 'N', 205409 ,4535545 ,
+# 'O', 105700 ,4729266 ,
+# 'P', 144239 ,1255579 ,
+# 'Q', 11659 ,54221,
+# 'R', 146448 ,4137949 ,
+# 'S', 304971 ,4186210,
+# 'T', 325462 ,5507692 ,
+# 'U', 57488 ,1613323,
+# 'V', 31053 ,653370,
+# 'W', 107195 ,1015656 ,
+# 'X', 7578 ,123577,
+# 'Y', 94297 ,1062040 ,
+# 'Z', 5610, 66423]
+# first = 1
+# second = 2
+# lower_total = 0
+# upper_total = 0
+# while second <= len(x):
+# 	# print 'first', x[first], 'second', x[second]
+# 	upper_total += x[first]
+# 	lower_total += x[second]
+# 	# print "'" + str(x[first].lower()) + "'" + ':' + str(x[second]/652045.03) + ','
+# 	first += 3
+# 	second += 3
+
+# print upper_total
+# print lower_total
+
+# # total = 65204503/3527609
