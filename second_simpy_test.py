29c1ae09fb7609a79f73f076e418926faa6a77ee
spencerkee <spencerkee@users.noreply.github.com>
2026-01-16 23:16:29 +0000

    Get mostly working import of autogluon

--- second_simpy_test.py
+++ second_simpy_test.py
@@ -200,17 +200,17 @@ if __name__ == "__main__":
     # preprocess_input_text = preprocess_input_text(TEXT_INPUT)
     # get_distance_for_chromosome(CHROMOSOME, preprocess_input_text)
 
-    # from autogluon.tabular import TabularDataset, TabularPredictor
+    from autogluon.tabular import TabularDataset, TabularPredictor
 
-    # data_url = "https://raw.githubusercontent.com/mli/ag-docs/main/knot_theory/"
-    # train_data = TabularDataset(f"{data_url}train.csv")
-    # train_data.head()
-    # label = "signature"
-    # train_data[label].describe()
-    # predictor = TabularPredictor(label=label).fit(train_data)
-    # test_data = TabularDataset(f"{data_url}test.csv")
+    data_url = "https://raw.githubusercontent.com/mli/ag-docs/main/knot_theory/"
+    train_data = TabularDataset(f"{data_url}train.csv")
+    train_data.head()
+    label = "signature"
+    train_data[label].describe()
+    predictor = TabularPredictor(label=label).fit(train_data)
+    test_data = TabularDataset(f"{data_url}test.csv")
 
-    # y_pred = predictor.predict(test_data.drop(columns=[label]))
-    # y_pred.head()
-    # predictor.evaluate(test_data)
-    # predictor.leaderboard(test_data)
+    y_pred = predictor.predict(test_data.drop(columns=[label]))
+    y_pred.head()
+    predictor.evaluate(test_data)
+    predictor.leaderboard(test_data)
